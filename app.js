56868eb8292f408eaad1843266c520c7638f9838
Luis Brime <brimegmz@gmail.com>
2019-04-02 20:56:23 -0600

    babel and nodemon added

--- app.js
+++ app.js
@@ -3,7 +3,20 @@ import db from './db';
 
 const app = express();
 
+app.get('/api/tequileros', (req, res) => {
+    var result = [];
+    var data = db.tequileros;
+    
+    for (var i = 0; i < data.length; i++) {
+        result.push(data[i].name);
+    }
 
+    res.status(200).send({
+        success: 'true',
+        message: 'tequileros retrieved successfuly',
+        tequileros: result
+    });
+});
 
 const PORT = 5000;
 app.listen(PORT, () => {
--- db.js
+++ db.js
@@ -1,134 +1,132 @@
-const db = [
-    {
-        tequileros: [
-            {
-                name: 'Sauza Tequila',
-                tequilas: [
-                    {
-                        marca: 'Tres Generaciones Plata',
-                        description: {
-                            color: '',
-                            aroma: '',
-                            taste: '',
-                            text: '',
-                        },
-                        image: '',
-                        sku: 'ttgp123456',
+const db = {
+    tequileros: [
+        {
+            name: 'Sauza Tequila',
+            tequilas: [
+                {
+                    marca: 'Tres Generaciones Plata',
+                    description: {
+                        color: '',
+                        aroma: '',
+                        taste: '',
+                        text: '',
                     },
-                    {
-                        marca: 'Tres Generaciones Reposado',
-                        description: {
-                            color: '',
-                            aroma: '',
-                            taste: '',
-                            text: '',
-                        },
-                        image: '',
-                        sku: 'ttgr234567',
+                    image: '',
+                    sku: 'ttgp123456',
+                },
+                {
+                    marca: 'Tres Generaciones Reposado',
+                    description: {
+                        color: '',
+                        aroma: '',
+                        taste: '',
+                        text: '',
                     },
-                    {
-                        marca: 'Tres Generaciones Añejo',
-                        description: {
-                            color: '',
-                            aroma: '',
-                            taste: '',
-                            text: '',
-                        },
-                        image: '',
-                        sku: 'ttga345678',
+                    image: '',
+                    sku: 'ttgr234567',
+                },
+                {
+                    marca: 'Tres Generaciones Añejo',
+                    description: {
+                        color: '',
+                        aroma: '',
+                        taste: '',
+                        text: '',
                     },
-                ],
-            },
-            {
-                name: 'IMGP',
-                tequilas: [
-                    {
-                        marca: 'Tequila Gutierrez Reposado',
-                        description: {
-                            color: '',
-                            aroma: '',
-                            taste: '',
-                            text: '',
-                        },
-                        image: '',
-                        sku: 'tgrimgp123',
-                    }
-                ],
-            },
-            {
-                name: 'Destilados Gómez',
-                tequilas: [
-                    {
-                        marca: '2090',
-                        description: {
-                            color: '',
-                            aroma: '',
-                            taste: '',
-                            text: '',
-                        },
-                        image: '',
-                        sku: 'dsgmz2090a',
+                    image: '',
+                    sku: 'ttga345678',
+                },
+            ],
+        },
+        {
+            name: 'IMGP',
+            tequilas: [
+                {
+                    marca: 'Tequila Gutierrez Reposado',
+                    description: {
+                        color: '',
+                        aroma: '',
+                        taste: '',
+                        text: '',
                     },
-                    {
-                        marca: 'Tequila Chapoteo',
-                        description: {
-                            color: '',
-                            aroma: '',
-                            taste: '',
-                            text: '',
-                        },
-                        image: '',
-                        sku: 'dsgmztqcho',
-                    }
-                ],
-            },
-            {
-                name: 'Tequilas Cariñocitos',
-                tequilas: [
-                    {
-                        marca: 'Tequieromucho',
-                        description: {
-                            color: '',
-                            aroma: '',
-                            taste: '',
-                            text: '',
-                        },
-                        image: '',
-                        sku: 'tqmtsca123',
+                    image: '',
+                    sku: 'tgrimgp123',
+                }
+            ],
+        },
+        {
+            name: 'Destilados Gómez',
+            tequilas: [
+                {
+                    marca: '2090',
+                    description: {
+                        color: '',
+                        aroma: '',
+                        taste: '',
+                        text: '',
                     },
-                ],
-            },
-        ],
-        users: [
-            {
-                user: 'LuisBrime',
-                pwd: 'elbrimes',
-                historial: [
-                    {
-                        sku: 'dsgmz2090a',
-                        fc: '',
-                        doc: '',
+                    image: '',
+                    sku: 'dsgmz2090a',
+                },
+                {
+                    marca: 'Tequila Chapoteo',
+                    description: {
+                        color: '',
+                        aroma: '',
+                        taste: '',
+                        text: '',
                     },
-                    {
-                        sku: 'tgrimgp123',
-                        fc: '',
-                        doc: '',
+                    image: '',
+                    sku: 'dsgmztqcho',
+                }
+            ],
+        },
+        {
+            name: 'Tequilas Cariñocitos',
+            tequilas: [
+                {
+                    marca: 'Tequieromucho',
+                    description: {
+                        color: '',
+                        aroma: '',
+                        taste: '',
+                        text: '',
                     },
-                ],
-            },
-            {
-                user: 'GenelJose',
-                pwd: 'naysek',
-                historial: [
-                    {
-                        sku: 'tqmtsca123',
-                        fc: '',
-                        doc: '',
-                    },
-                ],
-            },
-        ],
-    }
-];
+                    image: '',
+                    sku: 'tqmtsca123',
+                },
+            ],
+        },
+    ],
+    users: [
+        {
+            user: 'LuisBrime',
+            pwd: 'elbrimes',
+            historial: [
+                {
+                    sku: 'dsgmz2090a',
+                    fc: '',
+                    doc: '',
+                },
+                {
+                    sku: 'tgrimgp123',
+                    fc: '',
+                    doc: '',
+                },
+            ],
+        },
+        {
+            user: 'GenelJose',
+            pwd: 'naysek',
+            historial: [
+                {
+                    sku: 'tqmtsca123',
+                    fc: '',
+                    doc: '',
+                },
+            ],
+        },
+    ],
+};
 
 export default db;
